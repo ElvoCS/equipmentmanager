c4cf30318be4ae6b1a608cf4a3433bfbd9225bea
ElvoCS <55958363+ElvoCS@users.noreply.github.com>
2023-01-12 13:28:01 +0000

    setting up amplify auth
    setting up amplify auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "equipmentmanagera9f726db": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
